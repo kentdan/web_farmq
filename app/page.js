03eb2b332294d811832878308363423a7408ad00
Kent pc <kentdaniel18@gmail.com>
2024-06-22 10:03:00 +0800

    first commit

---
+++ app/page.js
@@ -0,0 +1,31 @@
+import { Suspense } from 'react'
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Problem from "@/components/Problem";
+import FeaturesAccordion from "@/components/FeaturesAccordion";
+import Pricing from "@/components/Pricing";
+import FAQ from "@/components/FAQ";
+import CTA from "@/components/CTA";
+import Footer from "@/components/Footer";
+import Testimonials3 from '@/components/Testimonials3';
+
+export default function Home() {
+  return (
+    <>
+      <Suspense>
+        <Header />
+      </Suspense>
+      <main>
+        <Hero />
+        <Problem />
+        <FeaturesAccordion />
+        <Pricing />
+        <FAQ />
+        <CTA />
+        <Testimonials3 />
+      </main>
+      <Footer />
+
+    </>
+  );
+}
