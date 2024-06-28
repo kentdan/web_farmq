3c5a6b78adcf44107c1e0feb64dfc41f6beb6733
Kent pc <kentdaniel18@gmail.com>
2024-06-28 15:30:06 +0800

    message not good on mobile

--- components/FeaturesAccordion.js
+++ components/FeaturesAccordion.js
@@ -199,9 +199,9 @@ const FeaturesAccordion = () => {
     >
       <div className="px-8">
         <h2 className="font-extrabold text-4xl lg:text-6xl tracking-tight mb-12 md:mb-24">
-          Start your healthy journey now and 
+          Start your healthy journey now 
           <span className="bg-neutral text-neutral-content px-2 md:px-4 ml-1 md:ml-1.5 leading-relaxed whitespace-nowrap">
-            Grow Fresh Veggies
+            Grow Veggies
           </span>
         </h2>
         <div className=" flex flex-col md:flex-row gap-12 md:gap-24">
